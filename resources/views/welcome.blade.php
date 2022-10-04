55416ec7fee40603a4dd51431e47fef2d2c4285b
AnwarSahid <anwarsahid28oktober@gmail.com>
2022-10-04 11:53:12 +0700

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Catalog</title>
+    <title>OPAC (Online Public Access Catalog Universitas Lampung)</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     @livewireStyles
